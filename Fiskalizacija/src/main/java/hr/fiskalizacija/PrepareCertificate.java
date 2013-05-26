a2d33c4c7bb36b7e0043d529b78e194c961062f2
jcutic <jcutic@gmail.com>
2013-05-27 00:14:58 +0200

    Dodatak

--- Fiskalizacija/src/main/java/hr/fiskalizacija/PrepareCertificate.java
+++ Fiskalizacija/src/main/java/hr/fiskalizacija/PrepareCertificate.java
@@ -23,42 +23,46 @@ public class PrepareCertificate{
     private static final String CERTIFICATE_TYPE_X509 = "X.509";
    
     
-	
 	public static int convertFromPKCSAndSSLToJKS(CertParameters certParameters){
 		int count = 0;
 		String aliasNameJKS = "";
 		String wrongPassword  = "Pogrešna lozinka .pfx datoteke";
 		try {
-			// Instanca keystore certifikata .p12/pfx formatu (PKCS12)
+			/** Instanca keystore certifikata .p12/pfx formatu (PKCS12) */
 			KeyStore keystorePFX = KeyStore.getInstance(KEYSTORE_TYPE_PKCS12);
 			
-			// Uèitavanje .pfx certifikata pomoæu šifre
-			keystorePFX.load(new FileInputStream(certParameters.getPathOfPFXCert() + certParameters.getNameOfPFXCert() + EXTENSION_OF_PFX), certParameters.getPasswdOfPFXCert().toCharArray());
+			/** Uèitavanje .pfx certifikata pomoæu šifre */
+			keystorePFX.load(new FileInputStream(certParameters.getPathOfPFXCert() + certParameters.getNameOfPFXCert() + 
+					EXTENSION_OF_PFX), certParameters.getPasswdOfPFXCert().toCharArray());
 			
 			Enumeration<String> en = keystorePFX.aliases();
-			// Prolaz kroz sve aliase
+			
+			/** Prolaz kroz sve aliase */
 			while (en.hasMoreElements()){
 				String alias = (String) en.nextElement();
 				System.out.println("Alias je: " + alias);
 
-				// Dohvat certifikata s javnim kljuèem koji ce biti zapisan u jks datoteku
+				/** Dohvat certifikata s javnim kljuèem koji ce biti zapisan u jks datoteku */
 				Certificate[] chain = keystorePFX.getCertificateChain(alias);
 
-				// Dohvati privatni kljuè certifikata koji æe biti zapisan u .jks datoteku
+				/** Dohvati privatni kljuè certifikata koji æe biti zapisan u .jks datoteku */
 				Key privateKey = keystorePFX.getKey(alias, certParameters.getPasswdOfPFXCert().toCharArray());
 				
 					if(certParameters.getAliasForPairJKSCert() == null){
 						certParameters.setAliasForPairJKSCert(certParameters.getOIBFromCert(keystorePFX, alias));
 					}
 					if(certParameters.getAliasCERCert() == null){
-						// ako nije postavljen Alias za SSL certifikat, bit æe SSL32344121 ( broj je oib)
+						
+						/** ako nije postavljen Alias za SSL certifikat, bit æe SSL32344121 ( broj je oib) */
 						certParameters.setAliasCERCert(CertParameters.CERTIFICATE_TYPE_SSL + certParameters.getOIBFromCert(keystorePFX, alias));	
 				}
 				
 				File fileJKS = new File(certParameters.getPathOfJKSCert() + certParameters.getNameOfJKSCert() + CertParameters.EXTENSION_OF_JKS);
-				// Instanca keystore certifikata .jks formatu
+				
+				/** Instanca keystore certifikata .jks formatu */
 				KeyStore keyStoreJKS = KeyStore.getInstance(CertParameters.KEYSTORE_TYPE_JKS);
-				// Ako ne postoji .jks file, potrebno ga je kreirati zajedno s putanjom direktorija
+				
+				/** Ako ne postoji .jks file, potrebno ga je kreirati zajedno s putanjom direktorija */
 				if (!fileJKS.exists()){
 				//	fileJKS.getParentFile().mkdirs();
 					fileJKS.createNewFile();
@@ -68,19 +72,19 @@ public class PrepareCertificate{
 					keyStoreJKS.load(new FileInputStream(certParameters.getPathOfJKSCert() + certParameters.getNameOfJKSCert() + CertParameters.EXTENSION_OF_JKS), certParameters.getPasswdOfJKSCert().toCharArray());
 				}
 				aliasNameJKS = certParameters.getAliasForPairJKSCert();
-				// Ako je u ovom trenutku brojaè veæi od 1 znaèi da ima više aliasa, pa ih je potrebno spremiti pod drugim nazivima: alias_brojac
-				// Ova situacija se ne bi smjela dogoditi prilikom fiskalizacije
+				/** Ako je u ovom trenutku brojaè veæi od 1 znaèi da ima više aliasa, pa ih je potrebno spremiti pod drugim nazivima: alias_brojac
+				    To se ne bi trebala dogoditi prilikom fiskalizacije, jer svaki korisnik ima jedan certifikat u svoj keystoreu */
 				if(count > 0){
 					aliasNameJKS = certParameters.getAliasForPairJKSCert() + "_" + (count+1);
 				}
 				keyStoreJKS.setKeyEntry(aliasNameJKS, privateKey, certParameters.getPasswdOfJKSCert().toCharArray(), chain);
 				
-				// Dio koji uèitava SSL certifikat i postavlja ga u JKS
+				/** Dio koji uèitava SSL certifikat i postavlja ga u JKS */
 				CertificateFactory certFactory = CertificateFactory.getInstance(CERTIFICATE_TYPE_X509);
 				Certificate certForSSL = certFactory.generateCertificate(new BufferedInputStream(new FileInputStream(certParameters.getPathOfCERCert() + certParameters.getNameOfCERCert() + EXTENSION_OF_CER)));
 				keyStoreJKS.setCertificateEntry(certParameters.getAliasCERCert(), certForSSL);
 				
-				// Zapiši uèitani certifikat sa željenim aliasom, šifrom, privatnim kljuèem i certifikatom u .jks datoteku
+				/** Zapiši uèitani certifikat sa željenim aliasom, šifrom, privatnim kljuèem i certifikatom u .jks datoteku */
 				keyStoreJKS.store(new FileOutputStream(certParameters.getPathOfJKSCert() + certParameters.getNameOfJKSCert() + CertParameters.EXTENSION_OF_JKS), certParameters.getPasswdOfJKSCert().toCharArray());
 				count++;
 			}
@@ -93,10 +97,5 @@ public class PrepareCertificate{
 		}
 		return count;
 	}
-	
-	
-	
-	
-	
 	
 }
